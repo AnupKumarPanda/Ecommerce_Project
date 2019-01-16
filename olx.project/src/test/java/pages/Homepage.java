c6b4815c27670d63a7f988a3e7d95a2079c2c0ad
anupkumarpanda <anupkumarpanda024@gmail.com>
2019-01-16 19:00:02 +0530

    some sdditional codes on submita free ad link

--- olx.project/src/test/java/pages/Homepage.java
+++ olx.project/src/test/java/pages/Homepage.java
@@ -3,17 +3,29 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Homepage 
 {
+	static WebDriverWait w;
 	static WebDriver  driver=null;
-	@Test()
+	@BeforeClass
+	public static void launchMethod()
+	{
+		System.setProperty("webdriver.chrome.driver","D:\\Softwares\\BrowserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		w =new WebDriverWait(driver,20);
+		driver.manage().window().maximize();
+		driver.get("https://www.olx.in");
+	}
+	@Test(priority=1)
 	public static void method()
 	{
 		System.setProperty("webdriver.chrome.driver","D:\\Softwares\\BrowserDrivers\\chromedriver.exe");
@@ -33,8 +45,17 @@ public class Homepage
 			Assert.assertTrue(false);
 		}
 	}
-	@AfterClass
+	@Test(priority=2)
 	public static void method2()
+	{
+		//to test submit a free ad link
+		w.until(ExpectedCondition<ChromeDriver>
+		driver.findElement(By.xpath("//*[text()='Submit a Free Ad']")).click();
+		WebElement fad =
+		
+	}
+	@AfterClass
+	public static void closeMethod()
 	{
 		driver.close();
 	}
